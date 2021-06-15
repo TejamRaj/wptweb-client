105511f6417780e1f7a102cd9f779072b76b7d7c
Raj Tejam <rajendra.tejam@outlook.com>
2021-06-15 15:31:29 +0530

    framework-build and cleanup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://localhost:44353/api/',
+  openIdConnectSettings: {
+    authority: 'https://localhost:5001/',
+    client_id: 'wpt-webclient',
+    redirect_uri: 'https://localhost:4200/signin-oidc',
+    scope: 'openid profile roles',
+    response_type: 'id_token token',
+    post_logout_redirect_uri:'https://localhost:4200/'
+  }
 };
 
 /*
