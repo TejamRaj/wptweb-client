148e3b53877fc3f6fb9e5daab45c890d09151253
Raj Tejam <rajendra.tejam@outlook.com>
2021-06-15 15:33:21 +0530

    cleanup and bootstraping wpt-webclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { OidConnectService } from './shared/OidConnect.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wptweb-client';
+  title = ' WorkPlace On Tour :)';
+
+ @Input() oidcClient:OidConnectService
+
+  constructor(private openIdConnectService: OidConnectService) {
+    this.oidcClient=openIdConnectService;
+  }
+
+   ngOnInit(){
+   
+  var path= window.location.pathname;
+    
+     if(path!="/signin-oidc"){
+
+        if(!this.openIdConnectService.userAvailable)
+          this.openIdConnectService.triggerSignIn();
+      
+   }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { RequireAuthenticatedUserRouteGuardService } from './shared/require-authenticated-user-route-guard.service';
+import { OidConnectService } from './shared/OidConnect.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninOidcComponent } from './signin-oidc/signin-oidc.component';
+import { WorkplacesOnTourComponent } from './workplaces-on-tour/workplaces-on-tour.component';
+import { MyTourWorkplacesComponent } from './my-tour-workplaces/my-tour-workplaces.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninOidcComponent,
+    WorkplacesOnTourComponent,
+    MyTourWorkplacesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
-  providers: [],
+  providers: [OidConnectService,
+  RequireAuthenticatedUserRouteGuardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
